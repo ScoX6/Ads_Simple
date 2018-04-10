91eee9dc9d13c0f5cf0b447df904197a211abca7
Scorpio.X <1016667005@qq.com>
2018-04-10 17:30:38 +0800

    Initial Commit

---
+++ Ads_Simple/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ads_Simple
+//
+//  Created by 熊智凡 on 2018/4/10.
+//  Copyright © 2018年 Scorpio.X. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
